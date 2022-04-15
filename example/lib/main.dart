0736105770c1800ce223eff43cefbc2b29e9e507
anabebe1020 <rabe1020.biz@gmail.com>
2022-04-15 21:48:17 +0900

    add, example pj.

---
+++ example/lib/main.dart
@@ -0,0 +1,92 @@
+import 'package:anabebe_packages/anabebe_packages.dart';
+import 'package:example/test_view.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'anabebe_packages_demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blueGrey,
+      ),
+      home: const TestHomePage(),
+    );
+  }
+}
+
+class TestHomePage extends StatefulWidget {
+  const TestHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<TestHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<TestHomePage> {
+  int _currentIndex = 0;
+  final _pageController = PageController();
+  late List<Widget> _pageList;
+
+  /// get instance
+  final logger = Logger();
+
+  @override
+  void initState() {
+    _pageList = [
+      const SelectBoxTestPage(),
+      const SelectBoxTestPage(),
+    ];
+
+    /// set domain(page) name
+    logger.setup('main');
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('anabebe_packages Test View'),
+        titleTextStyle: const TextStyle(fontSize: 18.0, color: Colors.blueGrey),
+        backgroundColor: Colors.white12,
+        elevation: 0,
+      ),
+      body: PageView(
+        controller: _pageController,
+        onPageChanged: (index) {
+          logger.log('onPageChanged $index');
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+        children: _pageList,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        unselectedItemColor: Colors.grey,
+        showUnselectedLabels: true,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'SelectBox',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: 'Image',
+          ),
+        ],
+        currentIndex: _currentIndex,
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
---
+++ example/lib/test_view.dart
@@ -0,0 +1,46 @@
+import 'package:anabebe_packages/anabebe_packages.dart';
+import 'package:flutter/material.dart';
+
+class SelectBoxTestPage extends StatefulWidget {
+  const SelectBoxTestPage({Key? key}) : super(key: key);
+
+  @override
+  State<SelectBoxTestPage> createState() => _SelectBoxTestPageState();
+}
+
+class _SelectBoxTestPageState extends State<SelectBoxTestPage> {
+  late Logger logger;
+
+  late List<String> _items;
+  late String _currentItem;
+
+  @override
+  initState() {
+    _items = ['0', '1', '2', '3'];
+    _currentItem = _items.first;
+
+    ///
+    logger = Logger();
+    logger.setup('TestView');
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    logger.log('build');
+
+    return Scaffold(
+      body: Container(
+        child: Column(
+          children: [
+            SelectBox(
+              items: _items,
+              currentItem: _currentItem,
+              onChanged: (_) {},
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
