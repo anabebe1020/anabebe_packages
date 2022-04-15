54890a7fd433ba82296033047890fffda5a69f45
anabebe1020 <rabe1020.biz@gmail.com>
2022-04-15 21:47:43 +0900

    fix, test code.

--- test/anabebe_packages_test.dart
+++ test/anabebe_packages_test.dart
@@ -1,11 +1,10 @@
-import 'package:flutter_test/flutter_test.dart';
-
 import 'package:anabebe_packages/anabebe_packages.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   test('adds one to input values', () {
     final logger = Logger();
-    logger.setup('TEST');
-    logger.log('test message');
+    logger.setup('main');
+    logger.log('start');
   });
 }
